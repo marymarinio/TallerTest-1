8abf64f67cf8c265abc0977c58dca57642d298f1
Nahuel <cristian.nina@uab.edu.bo>
2022-10-20 14:48:53 -0400

    Agrgando nuevo ejercicio

---
+++ nuevobranch/ejercicio.py
@@ -0,0 +1,8 @@
+n1=int(input("Ingrese numero:"))
+n2=int(input("Ingrese otro numero:"))
+if n1 < n2:
+    print ("Es mayor",n2)
+elif n2 < n1:
+    print ("Es mayor",n1)
+else:
+    print ("Son iguales")
