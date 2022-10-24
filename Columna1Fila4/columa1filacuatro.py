8b9f910111e58c2ba391b789b14cf79882dc09a9
Nahuel <cristian.nina@uab.edu.bo>
2022-10-24 14:35:12 -0400

    Ejercicios

---
+++ Columna1Fila4/columa1filacuatro.py
@@ -0,0 +1,14 @@
+numero=int(input("Ingrese un numero:"))
+numco1=0
+while numco1 <=numero:
+    fila=numco1
+    while (fila<numero):
+        print (fila)
+        fila=fila+1
+    print(numco1)
+    numco1=numco1+1
+    print("\t")
+
+#Nahuel Nina 
+#Luis Luna
+#Daniel Omonte
