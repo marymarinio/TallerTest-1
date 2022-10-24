3c8cec1dd1597a73d0ae5b21ecb4f4d508962f25
JuankDev <11jcat11@gmail.com>
2022-10-24 14:30:28 -0400

    Presentando el ejericio numero 2 de la clase del lunes

---
+++ Col2Fila5/Ejercicio2.py
@@ -0,0 +1,10 @@
+"""
+    Juan Carlos Aruni
+    Cristian Ledezma
+    Daniel Huchani
+"""
+numero = int(input("Ingrese un numero: ")) 
+suma_numero=""
+for numero in range(1,numero+1):
+    suma_numero += str(numero) + " "
+    print(suma_numero)
