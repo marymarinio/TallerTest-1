14559cf8cbc43c5e1f47af4ec80f9625a4b79c27
Edil <edil.zapata@uab.edu.bo>
2022-10-24 14:35:28 -0400

    Subiendo practica

---
+++ Colum_1_Fila_5/ejercicio.py
@@ -0,0 +1,20 @@
+#Edil Zapoata, Cristhian Condori, Mari Mariño
+
+numero=int(input("Ingrese un numero: "))
+lista=[]
+#lista.append(numero)
+cont=1
+while cont !=0:
+     for i in lista:
+        print(lista)
+        cont=i-1
+        print(cont)
+        #intentamos de diferentes formas 
+        
+while numero==0:
+    for i in lista:
+        x=numero-1
+        lista.append(x)
+        print(lista)
+
+
