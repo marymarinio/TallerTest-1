edc6664c69ae3bef01d70b46bed5bf9059f206d7
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2022-10-24 14:27:27 -0400

    Agregando nombres de los integrantes

--- Fila1Columna2.py
+++ Fila1Columna2.py
@@ -1,8 +1,11 @@
+#Josue Guzman Espejo
+#Facundo Miranda Yucra
+#Salomon Mena Lazarte
 numero=int(input("ingrese un numero: "))
 ListaNumeros=[]
 contador=0
 while contador<numero:
     contador=contador+1
     ListaNumeros.append(str(contador))
-    cadena="".join(ListaNumeros)
-    print(cadena)
+    EliminarCorchete="".join(ListaNumeros)
+    print(EliminarCorchete)
