7eb57a2aaaa747006dcb666f2f7f64ff75e87660
John <yhonelvis49@gmail.com>
2022-10-27 14:18:00 -0400

    los archivos .py se modificaron con separacion de cada numero por un *

--- Octubre/Octubre24/Ejercicio1.py
+++ Octubre/Octubre24/Ejercicio1.py
@@ -2,6 +2,6 @@ nroDeFilasPorTeclado = int(input("Ingrese un Nro: "))
 
 for cadaFila in range(nroDeFilasPorTeclado, 0, -1):
     for cadaColumna in range(1, cadaFila+1):
-        print(cadaColumna, end=" ")
+        print(cadaColumna, end="*")
     
     print("\n")
