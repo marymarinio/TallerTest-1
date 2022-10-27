2f92614c3363c5429fae341856e188759cab37bd
JuankDev <11jcat11@gmail.com>
2022-10-27 14:19:57 -0400

    Agregando un comentario sobre la descripcion del ejercicio

--- Octubre/Octubre24/Ejercicio2.py
+++ Octubre/Octubre24/Ejercicio2.py
@@ -1,3 +1,11 @@
+#Ejercicio media piramire decendente
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
 nroDeFilasPorTeclado = int(input("Ingrese un Nro: "))
 
 for cadaFila in range(nroDeFilasPorTeclado):
