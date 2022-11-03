5646fe25324d565dec3714dfb453de55f4c9366d
Marcos <marcos.blasquez@uab.edu.bo>
2022-11-03 19:35:09 +0000

    subiendo examen parcial

---
+++ Octubre/ExamenParcial-2/examen.py
@@ -0,0 +1,7 @@
+placas=input("Ingrese la placa: ")
+c=0
+for c in placas:
+    if valor == 0:
+    print("la placa ",placas," es correcta")
+    else:
+    print("la placa ",placas," es incorrecta")
